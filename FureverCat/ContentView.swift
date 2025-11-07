90fa995acf0ba2e7c83a3251fdcbefaba571ede3
XX <xiaoxiaxia88@gmail.com>
2025-11-07 16:15:04 -0500

    Initial Commit

---
+++ FureverCat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FureverCat
+//
+//  Created by xia on 11/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FureverCat/FureverCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FureverCatApp.swift
+//  FureverCat
+//
+//  Created by xia on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FureverCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
